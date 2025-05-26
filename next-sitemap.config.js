5446b30dfa065041ee8b563392e03b16b78e1d30
SREERAJ V RAJESH <119793289+cyberkutti-iedc@users.noreply.github.com>
2025-05-26 10:19:01 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://ecocee.in', // Replace with your website URL
-  generateRobotsTxt: true, // Generate a robots.txt file
-  generateIndexSitemap: true, // Optional: include an index sitemap
+  siteUrl: 'https://ecocee.in',
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: 'weekly',
+      priority: path === '/' ? 1.0 : 0.0, // Only homepage prioritized
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
 
 module.exports = config;
