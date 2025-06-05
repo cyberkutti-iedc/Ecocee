2b19e1578e7b09f57fdc9bf09ddc99e1310df0f5
SREERAJ V RAJESH <119793289+cyberkutti-iedc@users.noreply.github.com>
2025-06-05 15:11:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ const withTM = require('next-transpile-modules')(['@devnomic/marquee']);
 
 // Read environment variables from .env.local
 const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       'avatars.githubusercontent.com',
