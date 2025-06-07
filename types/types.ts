2a358ffbd7b7475af2bd13424ebea0408b3bfafd
cyberkutti-iedc <cyberkutti@gmail.com>
2025-06-07 13:29:23 +0530

    added types.ts

---
+++ types/types.ts
@@ -0,0 +1,24 @@
+export type InternFormData = {
+    fullName: string;
+    age: string;
+    dob: string;
+    email: string;
+    phone: string;
+    country: string;
+    state: string;
+    district: string;
+    address: string;
+    gender: string;
+    institutionName: string;
+    degree: string;
+    department: string;
+    areaOfInterest: string;
+    instituteId: string;
+    approvalLetter: string;
+    resume: string;
+    linkedinProfile: string;
+    githubProfile: string;
+    portfolio: string;
+    agreement: boolean;
+  };
+  
