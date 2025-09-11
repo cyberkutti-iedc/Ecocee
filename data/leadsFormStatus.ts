c932f9b7964fd5e926aa9155c4bf5610cd9c2750
cyberkutti-iedc <cyberkutti@gmail.com>
2025-09-11 12:32:35 +0530

    leads form is turned off

--- data/leadsFormStatus.ts
+++ data/leadsFormStatus.ts
@@ -1,4 +1,4 @@
 // Simple config for form status toggle
 export const leadsFormStatus = {
-  enabled: true // Set to false to disable the form
+  enabled: false // Set to false to disable the form
 };
